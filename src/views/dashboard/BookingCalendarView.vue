4bd1d5bde74df561155b7052f1c251c20643c1d3
ozo-vehe <johnnie.vehe@gmail.com>
2025-01-31 17:21:56 +0100

    replaced displayed calendar

--- src/views/dashboard/BookingCalendarView.vue
+++ src/views/dashboard/BookingCalendarView.vue
@@ -3,10 +3,12 @@
     <header class="my-7">
       <h1 class="text-4xl font-[700] uppercase">booking calendar</h1>
     </header>
-    <div class="min-h-[400px] flex items-center justify-center">
-      <span v-if="is_loading" class="block w-10 h-10 mx-auto rounded-full animate-spin border-x-2 border-gray-800"></span>
-      <VCalendar v-else @dayclick="handleGetDayBookingRequests" :attributes="attributes" :rows="2"
-        class="gap-6 booking_calendar" expanded />
+    <div class="h-[500px] max-w-[700px] mx-auto">
+      <span v-if="is_loading"
+        class="block w-10 h-10 mx-auto rounded-full animate-spin border-x-2 border-gray-800"></span>
+      <vue-cal v-else hide-view-selector xsmall :disable-views="['week']" events-count-on-year-view active-view="month" class="vuecal--green-theme"
+        :events="events" @cell-click="handleGetDayBookingRequests">
+      </vue-cal>
     </div>
   </main>
 </template>
@@ -16,85 +18,62 @@ import { onBeforeMount, ref, watch, type Ref } from 'vue'
 import { useBookingStore } from '@/stores/booking'
 import { storeToRefs } from 'pinia'
 import { useRouter } from 'vue-router'
+import VueCal from 'vue-cal'
+import 'vue-cal/dist/vuecal.css'
+
 console.log("Main dashboard")
 
 const bookingStore = useBookingStore();
 const { fetchBookings } = bookingStore;
 const { bookedDates, bookings } = storeToRefs(bookingStore);
-const bookedDatesArray: Ref<any[]> = ref([]);
+const booked_dates_array: Ref<any[]> = ref([]);
 
 const is_loading: Ref<boolean> = ref(false);
+const events: Ref<any[]> = ref([]);
 
 const router = useRouter();
 
 const handleGetDayBookingRequests = async (e: any) => {
-  const month = e.month.toString().padStart(2, '0');
-  const day = e.day.toString().padStart(2, '0');
-  const year = e.year;
-
-  const date = `${year}-${month}-${day}`;
-  router.push({ name: 'requests', params: { id: date } });
+  const date = e.toISOString().split('T')[0];
+  if(booked_dates_array.value.includes(date)) {
+    console.log('date is booked');
+    router.push({ name: 'requests', params: { id: date } });
+  }
 }
-
-const attributes = ref([
-  {
-    highlight: true,
-    dates: [
-      ...bookedDatesArray.value,
-    ],
-  },
-]);
-
-watch(bookedDates, (newVal, oldVal) => {
-  console.log('bookedDates changed');
-  bookedDatesArray.value = newVal;
-  const y = bookedDatesArray.value[0];
-  console.log(y)
-  attributes.value[0].dates = [
-    ...bookedDatesArray.value,
-  ];
-})
 
 onBeforeMount(async () => {
   is_loading.value = true;
   await fetchBookings()
-  is_loading.value = false;
-  console.log("Booking...")
-  console.log(bookings.value)
+  is_loading.value = false;;
+  bookings.value.forEach((booking: any) => {
+    booked_dates_array.value.push(booking.day);
+    console.log(booked_dates_array.value)
+    events.value.push({
+      start: `${booking.day} 12:00`,
+      end: `${booking.day} 12:00`
+    });
+
+  });
 })
 </script>
 
 <style scoped>
-/* Track */
-.table_container::-webkit-scrollbar {
-  height: 5px;
-  background-color: #f5f5f5;
-  border-radius: 15px;
+.vuecal__cell-events-count {
+  width: 18px;
+  height: 2px;
+  color: transparent;
 }
 
-/* Handle */
-.table_container::-webkit-scrollbar-thumb {
-  background: #888;
-  border-radius: 15px;
+/* Dot indicator */
+.vuecal__cell-events-count {
+  width: 4px;
+  min-width: 0;
+  height: 4px;
+  padding: 0;
+  color: transparent;
 }
 
-/* Handle on hover */
-.table_container::-webkit-scrollbar-thumb:hover {
-  background: #666;
-}
-
-/* Track on hover */
-.table_container::-webkit-scrollbar-track:hover {
-  background: #ddd;
-}
-
-.vc-container * {
-  color: #6366f1;
-  border: thin solid red !important;
-}
-
-.vc-week,
-.weekdays {
-  border: thin solid green !important;
-}
+/* Cell background indicator */
+.vuecal__cell--has-events {background-color: #fffacd;}
+.vuecal__cell-events-count {display: none;}
 </style>
